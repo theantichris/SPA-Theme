7ace808e1b6760742d3f7662820f3a1542adcf4d
Christopher Lamm <chris@theantichris.com>
2014-08-31 11:01:36 -0400

    Bower dependencies.

--- puppet/modules/bower/manifests/init.pp
+++ puppet/modules/bower/manifests/init.pp
@@ -5,4 +5,11 @@ class bower {
     unless => 'ls /usr/bin/bower',
     require => Package['nodejs'],
   }
+
+  exec {
+    'install-bower-packages':
+    command => 'bower install --allow-root',
+    cwd => '/vagrant',
+    require => Exec['install-bower'],
+  }
 }
