6ddb79a6b05743271a2a9d7b0ee9ae6d61ec3706
Christopher Lamm <chris@theantichris.com>
2014-09-01 09:01:58 -0400

    Enqueue Modernizr.

--- functions.php
+++ functions.php
@@ -1,2 +1,10 @@
 <?php
 
+function spa_theme_register_scripts_styles() {
+	$version = '0.0.0';
+
+	wp_register_script( 'modernizr', get_template_directory_uri() . '/bower_components/modernizr/modernizr.js', array( 'jquery' ), $version );
+	wp_enqueue_script( 'modernizr' );
+}
+
+add_action( 'wp_enqueue_scripts', 'spa_theme_register_scripts_styles' );
--- index.php
+++ index.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta http-equiv="X-UA-COMPATIBLE" content="IE=edge">
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="bower_components/normalize-css/normalize.css">
-    <link rel="stylesheet" href="style.css">
-    <script src="bower_components/modernizr/modernizr.js"></script>
-    <title><?php wp_title('|', true, 'right'); ?></title>
-    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-    <?php wp_head(); ?>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge">
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="bower_components/normalize-css/normalize.css">
+	<link rel="stylesheet" href="style.css">
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 
