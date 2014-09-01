5a9cde272121cc3420e6193b8338b5ffa332e6a1
Christopher Lamm <chris@theantichris.com>
2014-09-01 10:48:38 -0400

    Extracted header and footer.

---
+++ footer.php
@@ -0,0 +1,4 @@
+<footer>&copy; <?= date('Y') . ' - ' . get_bloginfo('name'); ?></footer>
+<?php wp_footer(); ?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,21 +1,5 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?> ng-app="spaThemeApp">
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge">
-	<meta name="description" content="">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
-	<link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/apple-touch-icon-precomposed.png" />
-	<title><?php wp_title( '|', true, 'right' ); ?></title>
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-	<?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
+<?php get_header(); ?>
 
 	<div ng-view></div>
 
-	<footer>&copy; <?= date('Y') . ' - ' . get_bloginfo('name'); ?></footer>
-	<?php wp_footer(); ?>
-</body>
-</html>
+<?php get_footer(); ?>
