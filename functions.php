19e6da86dac23612ff38ff9598cfd935f8a461eb
Christopher Lamm <chris@theantichris.com>
2014-09-01 09:09:49 -0400

    Removed unneeded dependency.

--- functions.php
+++ functions.php
@@ -3,11 +3,11 @@
 function spa_theme_register_scripts_styles() {
 	$version = '0.0.0';
 
-	wp_register_script( 'modernizr', get_template_directory_uri() . '/bower_components/modernizr/modernizr.js', array( 'jquery' ), $version );
+	wp_register_script( 'modernizr', get_template_directory_uri() . '/bower_components/modernizr/modernizr.js', array(), $version );
 	wp_enqueue_script( 'modernizr' );
 
-	wp_register_style('normalize-css', get_template_directory_uri() . '/bower_components/normalize-css/normalize.css', array(), $version);
-	wp_enqueue_style('normalize-css');
+	wp_register_style( 'normalize-css', get_template_directory_uri() . '/bower_components/normalize-css/normalize.css', array(), $version );
+	wp_enqueue_style( 'normalize-css' );
 }
 
 add_action( 'wp_enqueue_scripts', 'spa_theme_register_scripts_styles' );
