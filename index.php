58c444a4db3870eba485b81077a2b3b4b21defb0
Christopher Lamm <chris@theantichris.com>
2014-09-01 09:04:40 -0400

    Enqueue Normalize.

--- index.php
+++ index.php
@@ -5,8 +5,6 @@
 	<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge">
 	<meta name="description" content="">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="bower_components/normalize-css/normalize.css">
-	<link rel="stylesheet" href="style.css">
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<?php wp_head(); ?>
