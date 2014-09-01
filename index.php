cf4de4bba295101692f5b33ba3434840fab5f4af
Christopher Lamm <chris@theantichris.com>
2014-09-01 09:51:57 -0400

    Icons.

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 	<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge">
 	<meta name="description" content="">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
+	<link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/apple-touch-icon-precomposed.png" />
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<?php wp_head(); ?>
