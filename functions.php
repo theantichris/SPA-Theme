8bd506489649d8f330e2c155765a8cfeffab8690
Christopher Lamm <chris@theantichris.com>
2014-09-01 09:11:51 -0400

    Clean up.

--- functions.php
+++ functions.php
@@ -1,13 +1,16 @@
 <?php
 
 function spa_theme_register_scripts_styles() {
-	$version = '0.0.0';
+	$version   = '0.0.0';
+	$bower_dir = '/bower_components';
 
-	wp_register_script( 'modernizr', get_template_directory_uri() . '/bower_components/modernizr/modernizr.js', array(), $version );
+	wp_register_script( 'modernizr', get_template_directory_uri() . $bower_dir . '/modernizr/modernizr.js', array(), $version );
 	wp_enqueue_script( 'modernizr' );
 
-	wp_register_style( 'normalize-css', get_template_directory_uri() . '/bower_components/normalize-css/normalize.css', array(), $version );
+	wp_register_style( 'normalize-css', get_template_directory_uri() . $bower_dir . '/normalize-css/normalize.css', array(), $version );
 	wp_enqueue_style( 'normalize-css' );
+
+	wp_register_script( 'angular', get_template_directory_uri() . $bower_dir . '/angular/angular.min.js', array(), $version );
 }
 
 add_action( 'wp_enqueue_scripts', 'spa_theme_register_scripts_styles' );
