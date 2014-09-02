ffa7329d7a66031ca16eb8cbbc4eb078f78eb3ac
Christopher Lamm <chris@theantichris.com>
2014-09-02 16:15:40 -0400

    Formatting.

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,19 @@
 	spaThemeApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 		$locationProvider.html5Mode(true);
 
-		$routeProvider
-			.when('/', {templateUrl: themeDir + '/partials/main.html', controller: 'IndexCtrl'})
-			.when('/blog/:slug', {templateUrl: themeDir + '/partials/postDetail.html', controller: 'PostDetailCtrl'})
-			.when('/:slug', {templateUrl: themeDir + '/partials/pageDetail.html', controller: 'PageDetailCtrl'});
+		$routeProvider.when('/', {
+			templateUrl: themeDir + '/partials/main.html',
+			controller : 'IndexCtrl'
+		});
+
+		$routeProvider.when('/blog/:slug', {
+			templateUrl: themeDir + '/partials/postDetail.html',
+			controller : 'PostDetailCtrl'
+		});
+
+		$routeProvider.when('/:slug', {
+			templateUrl: themeDir + '/partials/pageDetail.html',
+			controller : 'PageDetailCtrl'
+		});
 	}]);
 })();
