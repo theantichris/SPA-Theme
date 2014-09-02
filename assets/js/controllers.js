b0eb32ad287aa72fdae940800cc250072757284f
Christopher Lamm <chris@theantichris.com>
2014-09-02 13:06:40 -0400

    Changed for wp-api.

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -4,23 +4,23 @@
 	var spaThemeControllers = angular.module('spaThemeControllers', []);
 
 	spaThemeControllers.controller('IndexCtrl', ['$scope', '$http', function ($scope, $http) {
-		$http.get('/api/get_posts').success(function (data) {
-			$scope.posts = data.posts;
+		$http.get('/wp-json/posts').success(function (data) {
+			$scope.posts = data;
 		});
 
-		$http.get('/api/get_posts?post_type=page').success(function (data) {
-			$scope.pages = data.posts;
+		$http.get('/wp-json/pages').success(function (data) {
+			$scope.pages = data;
 		});
 	}]);
 
 	spaThemeControllers.controller('PostDetailCtrl', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
-		$http.get('/api/get_post?slug=' + $routeParams.slug).success(function (data) {
+		$http.get('/wp-json/posts?slug=' + $routeParams.slug).success(function (data) {
 			$scope.post = data.post;
 		});
 	}]);
 
 	spaThemeControllers.controller('PageDetailCtrl', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
-		$http.get('/api/get_post?post_type=page&slug=' + $routeParams.slug).success(function (data) {
+		$http.get('/wp-json/pages?slug=' + $routeParams.slug).success(function (data) {
 			$scope.post = data.post;
 		});
 	}]);
